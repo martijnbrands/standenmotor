42b6fc765e754e47c0d5265fb92d4e743eb28871
martijnbrands <nijtrambra@hotmail.com>
2018-07-05 14:56:53 +0200

    Service worker

--- app/js/main.js
+++ app/js/main.js
@@ -4,3 +4,15 @@ $(document).ready(function(){
 	$('select').formSelect();
   
 });
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
