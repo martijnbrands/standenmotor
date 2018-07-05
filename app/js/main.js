32b1a311d7ea108b1e64b7bba91f7438f035893b
martijnbrands <nijtrambra@hotmail.com>
2018-07-05 15:02:53 +0200

    remove SW

--- app/js/main.js
+++ app/js/main.js
@@ -2,17 +2,4 @@ $(document).ready(function(){
 	$('.collapsible').collapsible();
 	$('.tabs').tabs();
 	$('select').formSelect();
-  
 });
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/sw.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
-  });
-}
