cf181a2c7ebcff79c7a43e80e8478cb292d4c716
martijnbrands <martijn.brands@in10.nl>
2019-01-08 17:24:58 +0100

    Login in Router.js

--- src/router.js
+++ src/router.js
@@ -3,6 +3,7 @@ import Router from "vue-router";
 import Score from "./views/Score.vue";
 import Drivers from "./views/Drivers.vue";
 import Arbiters from "./views/Arbiters.vue";
+import Login from "./views/Login.vue";
 
 Vue.use(Router);
 
@@ -24,6 +25,11 @@ export default new Router({
       path: "/fluitschema",
       name: "Arbiters",
       component: Arbiters
+    },
+    {
+      path: "/login",
+      name: "login",
+      component: Login
     }
   ]
 });
