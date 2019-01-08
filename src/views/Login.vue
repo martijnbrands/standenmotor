704ed5aeb1f4379a639fb01b0d69d0ea643e51ea
martijnbrands <martijn.brands@in10.nl>
2019-01-08 17:34:45 +0100

    Login input fields

---
+++ src/views/Login.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <v-subheader class="page_title">Login</v-subheader>
+    <v-form ref="form" lazy-validation>
+      <v-text-field label="E-mail"></v-text-field>
+      <v-text-field
+        :append-icon="show1 ? 'visibility_off' : 'visibility'"
+        :type="show1 ? 'text' : 'password'"
+        name="input-10-1"
+        label="Password"
+        @click:append="show1 = !show1"
+      ></v-text-field>
+      <v-btn color="primary">Login</v-btn>
+    </v-form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      show1: false,
+      rules: {
+        required: value => !!value || "Verplicht."
+      }
+    };
+  }
+};
+</script>
+
+
+
+<style lang="sass">
+.page_title
+  padding: 0
+</style>
