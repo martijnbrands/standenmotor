1f5a803c6a6ce46be06b719678c038680e73ae1e
martijnbrands <martijn.brands@in10.nl>
2019-01-09 15:44:19 +0100

    Cleanup DriverSchedule

--- src/components/DriversSchedule.vue
+++ src/components/DriversSchedule.vue
@@ -1,28 +1,4 @@
 <template>
-  <!-- <v-card v-if="drivers.length > 0">
-    <v-card-text class="py-0">
-      <v-timeline align-top dense v-for="driver in drivers" :key="driver.id">
-        <v-timeline-item color="warning" small>
-          <v-layout wrap pt-3>
-            <v-flex xs5>
-              <strong>{{ moment.unix(driver.time.seconds).format("DD-MM-YYYY") }}</strong>
-            </v-flex>
-            <v-flex>
-              <strong class="d-block pb-3">{{ driver.team }}</strong>
-              <div
-                v-for="driverName in driver.driver"
-                :key="driverName.id"
-                class="mb-2"
-              >{{ driverName }}</div>
-            </v-flex>
-          </v-layout>
-        </v-timeline-item>
-      </v-timeline>
-    </v-card-text>
-  </v-card>
-  <div v-else>
-    <v-progress-linear :indeterminate="true"></v-progress-linear>
-  </div>-->
   <v-card v-if="drivers.length > 0">
     <v-card-text class="py-0">
       <v-timeline align-top dense v-for="driver in drivers" :key="driver.id">
