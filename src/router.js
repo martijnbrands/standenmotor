04a7eed6930e441699ff17fc5ab9a985500c2014
martijnbrands <nijtrambra@hotmail.com>
2019-01-13 12:04:31 +0100

    remove login from router

--- src/router.js
+++ src/router.js
@@ -3,7 +3,6 @@ import Router from "vue-router";
 import Score from "./views/Score.vue";
 import Drivers from "./views/Drivers.vue";
 import Arbiters from "./views/Arbiters.vue";
-import Login from "./views/Login.vue";
 
 Vue.use(Router);
 
@@ -25,11 +24,6 @@ export default new Router({
       path: "/fluitschema",
       name: "Arbiters",
       component: Arbiters
-    },
-    {
-      path: "/login",
-      name: "login",
-      component: Login
     }
   ]
 });
