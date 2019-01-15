788baa06cd6a81bd1a1048cfa5451d05ac345af8
martijnbrands <martijn.brands@in10.nl>
2019-01-15 11:15:05 +0100

    Menu icon

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,6 +5,20 @@
         <div class="body-1">Doelpunten & Assists</div>
       </v-toolbar-title>
     </router-link>
+    <v-spacer></v-spacer>
+    <v-menu bottom left origin="center center" transition="slide-y-transition">
+      <v-btn slot="activator" dark icon>
+        <v-icon>more_vert</v-icon>
+      </v-btn>
+
+      <v-list>
+        <v-list-tile>
+          <router-link to="/login">
+            <v-list-tile-title>Log-in</v-list-tile-title>
+          </router-link>
+        </v-list-tile>
+      </v-list>
+    </v-menu>
   </v-toolbar>
 </template>
 
