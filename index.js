7e5229490031fb25774bcbecf27018098d5a9193
martijnbrands <nijtrambra@hotmail.com>
2020-09-12 19:40:00 +0200

    Change server port

--- index.js
+++ index.js
@@ -30,6 +30,6 @@ app.use('/api/matches', matchesRoute)
 
 app.use(express.static(path.join(__dirname, "public")))
 
-const port = process.env.SERVER_PORT || 3000;
+const port = process.env.SERVER_PORT || 5000;
 app.listen(port, () => console.log(`Server up and running on port ${port}`))
 
