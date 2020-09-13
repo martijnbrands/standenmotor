69541919b72cc53a3bb24b789f0ec769da7b69cf
martijnbrands <nijtrambra@hotmail.com>
2020-09-13 10:30:18 +0200

    if statement

--- server/index.js
+++ server/index.js
@@ -4,9 +4,11 @@ const dotenv = require('dotenv');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
-const serveStatic = require('serve-static')
 const path = require('path');
 dotenv.config();
+
+const PORT = process.env.SERVER_PORT || 3000;
+
 
 // Connect to Database
 mongoose.connect(process.env.DATABASE_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false}, () => 
@@ -29,11 +31,10 @@ app.use('/api/user', authRoute)
 app.use('/api/players', playersRoute)
 app.use('/api/matches', matchesRoute)
 
-//here we are configuring dist to serve app files
-app.use('/', express.static((__dirname + '/public')))
-//Handle SPA
-app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'))
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static('public'))
+}
 
-const PORT = process.env.SERVER_PORT || 3000;
+
 app.listen(PORT, () => console.log(`Server up and running on port ${PORT}`))
 
