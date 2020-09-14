30fcbefea801455aed229045c8dc9ef2ec6a338a
martijnbrands <nijtrambra@hotmail.com>
2020-09-14 18:42:16 +0200

    Change app.get

--- server/index.js
+++ server/index.js
@@ -36,9 +36,9 @@ if(process.env.NODE_ENV === "production") {
     app.use('/', express.static(path.join(__dirname, '/public')))
 
     // this * route is to serve project on different page routes except root `/`
-    app.get(/.*/, function (req, res) {
-        res.sendFile(path.join(__dirname, '/public/index.html'))
-    })
+    // app.get(/.*/, function (req, res) {
+    //     res.sendFile(path.join(__dirname, '/public/index.html'))
+    // })
 }
 
 
